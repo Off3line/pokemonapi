f3483800830aa00facd4a15199cf2b07930cb394
Off3line <johnyten@hotmail.com>
2022-10-14 22:43:01 +0200

    added chakra ui install

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import Panel from "./pages/Panel";
 import RestService from "./services/RestService";
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 const App = () => {
 
 return  (
-    <div>
+    
+     <ChakraProvider>
         <Panel/>
-    </div>
-     );
+     </ChakraProvider>
+)
 }
 
 export default App
--- src/pages/Panel.js
+++ src/pages/Panel.js
@@ -1,9 +1,14 @@
+import { Text } from "@chakra-ui/react";
 import React, {useEffect} from "react";
-import restService from "../services/RestService";  
+import restService from "../services/RestService";
 const Panel = () => {
+    console.log('Hello from component');
+
+
 
     return(
         <div>
+            <Text fontSize='6xl'>Chakra UI</Text>
             <p>Hello World!</p>
         </div>
     );
