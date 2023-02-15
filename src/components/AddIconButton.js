a8fd44f5d91f235649e2021ae8af5800b5b8bb8a
Off3line <johnyten@hotmail.com>
2023-02-15 21:00:49 +0100

    made the button responsive

--- src/components/AddIconButton.js
+++ src/components/AddIconButton.js
@@ -3,22 +3,24 @@ import { AddIcon,MinusIcon } from '@chakra-ui/icons'
 import { useState } from "react";
 
 
-function AddIconButton({handleAddClickEvent,handleRemoveClickEvent, id}) {
+export default function AddIconButton({handleAddClickEvent,handleRemoveClickEvent, id}) {
     
 
+    const stackStyles = {
+        direction:'column',
+     
+
+    }
+
+    const buttonStyle = {
+        my: {base: 1, md: 2, lg: 50},
+    }
+
     return (
-        <Stack direction='column'>
-        <IconButton aria-label="add search field" icon={<AddIcon/>} onClick={handleAddClickEvent} id={id}/>
-        <IconButton aria-label="remove search field" icon={<MinusIcon/>} onClick={handleRemoveClickEvent}/>
+        <Stack sx={stackStyles}>
+        <IconButton aria-label="add search field" icon={<AddIcon/>} onClick={handleAddClickEvent} sx={buttonStyle} id={id} size={{base: 'md' , lg: 'lg'}}/>
+        <IconButton aria-label="remove search field" icon={<MinusIcon/>} onClick={handleRemoveClickEvent} sx={buttonStyle} size={{base: 'md' , lg: 'lg'}}/>
         </Stack>
        
     );
 }
-
-
-
-
-
-
-
-export default AddIconButton;
